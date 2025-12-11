9444d4f60e9dad0c5cdc2fd1409d326d5ce5e651
Carl Nathan <2540394@edu.vaniercollege.qc.ca>
2025-12-11 00:54:13 -0500

    Add method calcAssignmentAvg() and method toString()

--- src/main/java/org/carl/Assignment.java
+++ src/main/java/org/carl/Assignment.java
@@ -9,4 +9,23 @@ public class Assignment {
     private List<Integer> scores;
 
     private static int nextId;
+
+    private void calcAssignmentAvg() {
+        double sum = 0;
+
+        for (int score : scores) {
+            sum += score;
+        }
+
+        double average = sum / scores.size();
+    }
+
+    @Override
+    public String toString() {
+        return "Assignment{" +
+                String.format("assignmentId='" + nextId++) + '\'' +
+                ", assignmentName='" + assignmentName + '\'' +
+                ", weight=" + weight +
+                '}';
+    }
 }
