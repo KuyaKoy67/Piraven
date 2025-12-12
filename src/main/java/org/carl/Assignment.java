32d0f2fef55061b322295b642b821363cd6907f7
2540394 <2540394@edu.vaniercollege.qc.ca>
2025-12-12 13:20:24 -0500

    Add method generateRandomScore()

--- src/main/java/org/carl/Assignment.java
+++ src/main/java/org/carl/Assignment.java
@@ -20,6 +20,33 @@ public class Assignment {
         double average = sum / scores.size();
     }
 
+    public void generateRandomScore() {
+        Random rand = new Random();
+
+        int randomValue = rand.nextInt(0, 10 + 1);
+        int randomScore;
+
+        if (randomValue == 0) {
+            randomScore = rand.nextInt(0, 60 + 1);
+
+        } else if (randomValue == 1 || randomValue == 2) {
+            randomScore = rand.nextInt(60, 70 + 1);
+
+        } else if (randomValue == 3 || randomValue == 4) {
+            randomScore = rand.nextInt(70, 80 + 1);
+
+        } else if (randomValue == 5 || randomValue == 6 ||
+                randomValue == 7 || randomValue == 8) {
+            randomScore = rand.nextInt(70, 80 + 1);
+
+        } else {
+            randomScore = rand.nextInt(90, 100 + 1);
+
+        }
+
+        scores.add(randomScore);
+    }
+
     @Override
     public String toString() {
         return "Assignment{" +
