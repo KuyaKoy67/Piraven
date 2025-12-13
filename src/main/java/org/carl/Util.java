74f87785a236862d5450774a1fff26e8586790a6
Carl Nathan <2540394@edu.vaniercollege.qc.ca>
2025-12-13 00:05:59 -0500

    Add method toTitleCase(String str)

--- src/main/java/org/carl/Util.java
+++ src/main/java/org/carl/Util.java
@@ -1,4 +1,24 @@
 package org.carl;
 
+import java.util.Arrays;
+
 public class Util {
+
+    public static String toTitleCase(String str) {
+        String[] words = str.split(" ");
+
+        for (String word : words) {
+            if (word == null) {
+                return null;
+            }
+
+            int spaceIdx = word.indexOf(" ");
+            String firstLetter = word.substring(0, 1).toUpperCase();
+            String otherLetters = word.substring(1, spaceIdx).toLowerCase();
+
+            return firstLetter + otherLetters + " ";
+        }
+
+        return Arrays.toString(words);
+    }
 }
